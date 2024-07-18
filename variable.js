6a993abcfeed2a58d7095d5b6523e41e8247259d
Ankul Awasthi <awasthi.ankul121@gmail.com>
2024-07-18 16:32:48 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,55 @@
+// Premitive Data type -: 
+/*
+1. Number
+2. Boolean
+3. String
+4. Undefined
+5. Null
+6. Bigint
+*/
+
+// Number Data type : 
+
+let num = 23;
+console.log(num);
+console.log(typeof num);
+num = 24;
+console.log(num);
+// let num = 25 --> We can't define same variable name again. 
+
+// Boolean 
+
+let bool = true;
+console.log(typeof bool);
+
+// String 
+
+var str = "Ankul";
+var str = "Anushka" // We can define same variable name again and again
+var str = "Ravi"
+
+console.log(str)
+console.log(typeof str); 
+
+// undefined
+
+var n;
+console.log(n);
+console.log(typeof n);
+
+// Null 
+
+var k = null;
+console.log(k);
+console.log(typeof k);
+
+//Bigint
+
+let x = BigInt("9999999999999999");
+
+console.log(x);
+console.log(typeof x);
+
+// Symbol
+
+
