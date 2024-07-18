f0471b9b16b31d4c7f4d092a90078b6362b07c6b
Ankul Awasthi <awasthi.ankul121@gmail.com>
2024-07-18 17:19:11 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,38 @@
+let a = 3;
+let b = 5;
+
+// addition
+
+let sum;
+sum = a+b;
+console.log(sum);
+
+// subtraction
+
+let diff;
+diff = a-b;
+console.log(diff);
+
+//multiplication
+
+let prod;
+prod = a*b;
+console.log(prod);
+
+//division
+
+let div;
+div = a/b;
+console.log(div);
+
+// modulo
+
+let rem;
+rem = a%b;
+console.log(rem);
+
+//Exponentiation (power operator)
+
+let pow;
+pow = a**b;
+console.log(pow);
