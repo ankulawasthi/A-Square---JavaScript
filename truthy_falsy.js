8e70dec106f47cda5e5e511b152385ff99224fb7
Ankul Awasthi <awasthi.ankul121@gmail.com>
2024-07-22 02:11:45 +0530

    truthy-falsy

---
+++ truthy_falsy.js
@@ -0,0 +1,10 @@
+// Truthy and Falsy: In javaScript everything is truth and false. 
+if(true){
+    console.log("its a true");
+}
+else if(false){
+      console.log("its a fase")
+}
+
+// Falsy value : false, 0 , -0 "", null, undefined, NaN
+// Truthy value  : EVERYTHING ELSE
