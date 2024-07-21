ca988e3d97bbe416fcea2de9eacdfebf1642a42c
Ankul Awasthi <awasthi.ankul121@gmail.com>
2024-07-22 00:28:23 +0530

    string

---
+++ string.js
@@ -0,0 +1,31 @@
+// String: String are text and sequence of character : 
+
+let name="Ankul";
+let num = "24";
+let chr = "A";
+let empty = ""; 
+name = "kapil"
+console.log(name);
+
+// Indices 
+
+let lastName = "Awasthi"; 
+console.log(lastName[0]);
+console.log(lastName.length);
+
+// concatenation 
+
+let firstName = "Ankul";
+let lName = "Awasthi"; 
+console.log(firstName + " " + lName);
+
+let n = 36;
+console.log(firstName + n);
+
+// Undefined : A variable that has not been assigned a value is of a type undefined. 
+
+let a; 
+
+// NUll : The null value represent the intention absence of any object value. 
+
+let k = null;
